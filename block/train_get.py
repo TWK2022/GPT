87acfae85f8531532adb25df864507021d335904
TWK <109967047+TWK2022@users.noreply.github.com>
2023-11-15 20:18:10 +0800

    Update train_get.py

--- block/train_get.py
+++ block/train_get.py
@@ -1,11 +1,10 @@
 import tqdm
 import torch
-import transformers
 from block.ModelEMA import ModelEMA
 from block.lr_get import adam
 
 
-def train_get(args, data_dict, model_dict, loss):
+def train_get(args, data_dict, model_dict):
     # 加载模型
     model = model_dict['model'].to(args.device, non_blocking=args.latch)
     # 学习率
@@ -15,16 +14,16 @@ def train_get(args, data_dict, model_dict, loss):
     if args.ema:
         ema.updates = model_dict['ema_updates']
     # 数据集
-    train_dataset = torch_dataset(args, data_dict['train_input'], data_dict['train_output'])
+    train_dataset = torch_dataset(args, data_dict['train_input'], model_dict['tokenizer'])
     train_shuffle = False if args.distributed else True  # 分布式设置sampler后shuffle要为False
     train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset) if args.distributed else None
     train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batch, shuffle=train_shuffle,
                                                    drop_last=True, pin_memory=args.latch, num_workers=args.num_worker,
-                                                   sampler=train_sampler)
+                                                   sampler=train_sampler, collate_fn=train_dataset.collate_fn)
     # 分布式初始化
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.local_rank],
                                                       output_device=args.local_rank) if args.distributed else model
-    for epoch in range(args.epoch):
+    for epoch in range(1, args.epoch + 1):
         # 训练
         print(f'\n-----------------------第{epoch}轮-----------------------') if args.local_rank == 0 else None
         model.train()
@@ -32,21 +31,22 @@ def train_get(args, data_dict, model_dict, loss):
         tqdm_show = tqdm.tqdm(
             total=len(data_dict['train_input']) // args.batch // args.device_number * args.device_number, postfix=dict,
             mininterval=0.2) if args.local_rank == 0 else None  # tqdm
-        for item, (input_ids, attention_mask, true_batch) in enumerate(train_dataloader):
-            input_ids = input_ids.to(args.device, non_blocking=args.latch)
-            attention_mask = attention_mask.to(args.device, non_blocking=args.latch)
-            true_batch = true_batch.to(args.device, non_blocking=args.latch)
+        for item, (input_ids_batch, attention_mask_batch, label_batch) in enumerate(train_dataloader):
+            input_ids_batch = input_ids_batch.to(args.device, non_blocking=args.latch)
+            attention_mask_batch = attention_mask_batch.to(args.device, non_blocking=args.latch)
+            label_batch = label_batch.to(args.device, non_blocking=args.latch)
             if args.amp:
                 with torch.cuda.amp.autocast():
-                    pred_batch = model(input_ids, attention_mask).logits
-                    loss_batch = loss(pred_batch, true_batch)
+                    pred_batch = model(input_ids=input_ids_batch, attention_mask=attention_mask_batch,
+                                       labels=label_batch)
+                    loss_batch = pred_batch.loss  # 当传入labels时模型内部会自动计算损失
                 optimizer.zero_grad()
                 args.amp.scale(loss_batch).backward()
                 args.amp.step(optimizer)
                 args.amp.update()
             else:
-                pred_batch = model(input_ids, attention_mask).logits
-                loss_batch = loss(pred_batch, true_batch)
+                pred_batch = model(input_ids=input_ids_batch, attention_mask=attention_mask_batch, labels=label_batch)
+                loss_batch = pred_batch.loss  # 当传入labels时模型内部会自动计算损失
                 optimizer.zero_grad()
                 loss_batch.backward()
                 optimizer.step()
@@ -64,28 +64,52 @@ def train_get(args, data_dict, model_dict, loss):
         train_loss = train_loss / (item + 1)
         print('\n| train_loss:{:.4f} | lr:{:.6f} |\n'.format(train_loss, optimizer.param_groups[0]['lr']))
         # 清理显存空间
-        del input_ids, attention_mask, true_batch, pred_batch, loss_batch
+        del input_ids_batch, attention_mask_batch, label_batch, pred_batch, loss_batch
         torch.cuda.empty_cache()
         # 保存
         if args.local_rank == 0:  # 分布式时只保存一次
-            if epoch % 5 == 0:
-                model.save_pretrained(f'save_epoch_{epoch}')
+            if epoch % 1 == 0:
+                save_name = f'save_epoch_{epoch}'
+                model_dict['tokenizer'].save_pretrained(save_name)
+                model.save_pretrained(save_name)
         torch.distributed.barrier() if args.distributed else None  # 分布式时每轮训练后让所有GPU进行同步，快的GPU会在此等待
 
 
 class torch_dataset(torch.utils.data.Dataset):
-    def __init__(self, args, input_data, output_data):
+    def __init__(self, args, input_data, tokenizer):
         self.input_data = input_data
-        self.output_data = output_data
-        self.tokenizer = transformers.BertTokenizer.from_pretrained(args.model)
+        self.tokenizer = tokenizer
+        self.eos_token = tokenizer.eos_token
+        self.pad_token_id = tokenizer.pad_token_id
+        self.prompt = 'You are a helpful assistant. 你是一个乐于助人的助手。'  # 提示词
+        self.template = ('[INST] <<SYS>>\n'
+                         '{prompt}\n'
+                         '<</SYS>>\n\n'
+                         '{instruction} [/INST]')  # 对话模型的输入需要经过特殊的处理
 
     def __len__(self):
         return len(self.input_data)
 
     def __getitem__(self, index):
-        input_dict = self.tokenizer(self.input_data[index], max_length=77, padding='max_length', truncation=True,
-                                    return_tensors='pt')
-        input_ids = input_dict['input_ids'].type(torch.int32).squeeze(0)
-        attention_mask = input_dict['attention_mask'].type(torch.int32).squeeze(0)
-        label = torch.tensor(self.output_data[index], dtype=torch.float32)  # 转换为tensor
+        sft_dict = self.input_data[index]
+        instruction = sft_dict['instruction'] + '\n' + sft_dict['input'] if sft_dict['input'] \
+            else sft_dict['instruction']
+        output = sft_dict['output'] + str(self.eos_token)
+        text_merge = self.template.format(prompt=self.prompt, instruction=instruction)  # 对话时的完整输入
+        input_encode = self.tokenizer.encode(text_merge, add_special_tokens=True, return_tensors='pt').squeeze(0)
+        output_encode = self.tokenizer.encode(output, add_special_tokens=False, return_tensors='pt').squeeze(0)
+        input_ids = torch.concat([input_encode, output_encode], dim=0)  # 训练时的完整输入input_ids
+        attention_mask = torch.full_like(input_ids, 1)  # attention_mask
+        label = torch.full_like(input_ids, -100)  # 不需要的地方变为-100
+        label[len(input_encode):] = output_encode  # 训练时的完整标签label
         return input_ids, attention_mask, label
+
+    def collate_fn(self, getitem_list):  # 自定义__getitem__的合并方式，填充数据然后合并为批量
+        input_ids_list = [_[0] for _ in getitem_list]
+        attention_mask_list = [_[1] for _ in getitem_list]
+        label_list = [_[2] for _ in getitem_list]
+        input_ids_batch = torch.nn.utils.rnn.pad_sequence(input_ids_list, batch_first=True,
+                                                          padding_value=self.pad_token_id)
+        attention_mask_batch = torch.nn.utils.rnn.pad_sequence(attention_mask_list, batch_first=True, padding_value=0)
+        label_batch = torch.nn.utils.rnn.pad_sequence(label_list, batch_first=True, padding_value=-100)
+        return input_ids_batch, attention_mask_batch, label_batch
