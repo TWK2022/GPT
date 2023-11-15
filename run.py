b1c71653411827e3befa1488dc411e52a7d02379
TWK <109967047+TWK2022@users.noreply.github.com>
2023-11-15 20:21:56 +0800

    Update run.py

--- run.py
+++ run.py
@@ -1,25 +1,26 @@
+import os
 import torch
 import argparse
 from block.data_get import data_get
 from block.model_get import model_get
-from block.loss_get import loss_get
 from block.train_get import train_get
 # -------------------------------------------------------------------------------------------------------------------- #
-# 数据data.csv格式：values768长度的特征向量，column为特征向量对应的中文文本
+# 数据格式：sft格式
+# [{'instruction':'A','input':'B','output':'C'},...]
 # -------------------------------------------------------------------------------------------------------------------- #
-# 分布式训练:
+# 分布式训练：
 # python -m torch.distributed.launch --master_port 9999 --nproc_per_node n run.py --distributed True
 # master_port为GPU之间的通讯端口，空闲的即可
 # n为GPU数量
 # -------------------------------------------------------------------------------------------------------------------- #
 # 设置
-parser = argparse.ArgumentParser(description='|clip中文文本模型微调|')
-parser.add_argument('--data_path', default=r'data.csv', type=str, help='|数据路径|')
-parser.add_argument('--model', default='chinese_model', type=str, help='|模型|')
-parser.add_argument('--epoch', default=26, type=int, help='|训练轮数|')
-parser.add_argument('--batch', default=10, type=int, help='|训练批量大小|')
+parser = argparse.ArgumentParser(description='|llama类大模型peft微调|')
+parser.add_argument('--data_path', default=r'data.json', type=str, help='|sft(.json)数据路径|')
+parser.add_argument('--weight', default='chinese-alpaca-2-1.3b', type=str, help='|tokenizer和模型文件夹位置|')
+parser.add_argument('--epoch', default=25, type=int, help='|训练轮数|')
+parser.add_argument('--batch', default=2, type=int, help='|训练批量大小|')
 parser.add_argument('--loss', default='mse', type=str, help='|损失函数|')
-parser.add_argument('--lr_start', default=0.000002, type=float, help='|初始学习率|')
+parser.add_argument('--lr_start', default=0.00001, type=float, help='|初始学习率|')
 parser.add_argument('--regularization', default='L2', type=str, help='|正则化，有L2、None|')
 parser.add_argument('--r_value', default=0.0005, type=float, help='|正则化的权重系数|')
 parser.add_argument('--device', default='cpu', type=str, help='|训练设备|')
@@ -50,13 +51,16 @@ if args.distributed:
     torch.distributed.init_process_group(backend="nccl")
     args.device = torch.device("cuda", args.local_rank)
 # -------------------------------------------------------------------------------------------------------------------- #
+# 初步检查
+assert os.path.exists(args.data_path), f'! data_path不存在:{args.data_path} !'
+assert os.path.exists(args.weight), f'! weight不存在:{args.weight} !'
+print(f'| 加载基座模型:{args.weight} |')
+# -------------------------------------------------------------------------------------------------------------------- #
 # 程序
 if __name__ == '__main__':
     # 数据
     data_dict = data_get(args)
     # 模型
     model_dict = model_get(args)
-    # 损失
-    loss = loss_get(args)
     # 训练
-    train_get(args, data_dict, model_dict, loss)
+    train_get(args, data_dict, model_dict)
