2ff9a449a916bbe07dd46df238061d7e2a10412b
TWK <109967047+TWK2022@users.noreply.github.com>
2024-01-03 09:38:14 +0800

    !

--- run.py
+++ run.py
@@ -24,32 +24,25 @@ from block.train_get import train_get
 # 设置
 parser = argparse.ArgumentParser(description='|llama类大模型微调:peft模型训练|')
 parser.add_argument('--data_path', default=r'data_demo.json', type=str, help='|sft(.json)数据路径|')
-parser.add_argument('--model_path', default='chinese-alpaca-2-1.3b', type=str,
-                    help='|原模型的tokenizer和模型文件夹位置|')
-parser.add_argument('--weight', default='last.pt', type=str,
-                    help='|会优先加载之前训练时保存的.pt模型继续训练，没有则新建peft再训练|')
-parser.add_argument('--save_pt', default=5, type=int,
-                    help='|每几轮保存一次完整的last.pt模型以便中断后继续训练，0为不保存|')
+parser.add_argument('--model_path', default='chinese-alpaca-2-1.3b', type=str, help='|原模型位置|')
+parser.add_argument('--weight', default='last.pt', type=str, help='|已有模型的位置，没有则新建peft再训练|')
+parser.add_argument('--save_pt', default=5, type=int, help='|每几轮保存一次last.pt模型以便中断后继续训练，0为不保存|')
 parser.add_argument('--wandb', default=False, type=bool, help='|是否使用wandb可视化|')
 parser.add_argument('--wandb_project', default='GPT', type=str, help='|wandb项目名称|')
 parser.add_argument('--wandb_name', default='train', type=str, help='|wandb项目中的训练名称|')
 parser.add_argument('--epoch', default=25, type=int, help='|训练轮数|')
 parser.add_argument('--batch', default=2, type=int, help='|训练批量大小|')
-parser.add_argument('--lr_start', default=0.00001, type=float,
-                    help='|初始学习率，训练中采用adam算法，前3轮有预热训练，基准为0.00001|')
+parser.add_argument('--lr_start', default=0.00001, type=float, help='|初始学习率，adam算法，3轮预热训练，基准为0.00001|')
 parser.add_argument('--lr_end_ratio', default=0.2, type=float, help='|最终学习率=lr_end_ratio*lr_start，基准为0.2|')
-parser.add_argument('--lr_adjust_num', default=20, type=int,
-                    help='|从初始学习率到最终学习率经过的调整次数，余玄下降法，一般小于总轮次|')
-parser.add_argument('--lr_adjust_threshold', default=0.97, type=float,
-                    help='|本轮训练损失大于上一轮损失的比例时才调整，基准为0.97|')
+parser.add_argument('--lr_adjust_num', default=20, type=int, help='|学习率下降调整次数，余玄下降法，要小于总轮次|')
+parser.add_argument('--lr_adjust_threshold', default=0.97, type=float, help='|本轮损失下降一定比例时才调整，基准为0.97|')
 parser.add_argument('--regularization', default='L2', type=str, help='|正则化，有L2、None|')
 parser.add_argument('--r_value', default=0.0005, type=float, help='|正则化的权重系数|')
 parser.add_argument('--device', default='cuda', type=str, help='|训练设备|')
 parser.add_argument('--latch', default=True, type=bool, help='|模型和数据是否为锁存，True为锁存|')
-parser.add_argument('--num_worker', default=0, type=int,
-                    help='|CPU在处理数据时使用的进程数，0表示只有一个主进程，一般为0、2、4、8|')
+parser.add_argument('--num_worker', default=0, type=int, help='|CPU处理数据的进程数，0表示只有一个主进程，一般为0、2、4、8|')
 parser.add_argument('--ema', default=True, type=bool, help='|使用平均指数移动(EMA)调整参数|')
-parser.add_argument('--amp', default=False, type=bool, help='|混合float16精度训练，CPU时不可用|')
+parser.add_argument('--amp', default=True, type=bool, help='|混合float16精度训练，CPU时不可用|')
 parser.add_argument('--distributed', default=False, type=bool, help='|单机多卡分布式训练，分布式训练时batch为总batch|')
 parser.add_argument('--local_rank', default=0, type=int, help='|分布式训练使用命令后会自动传入的参数|')
 args = parser.parse_args()
