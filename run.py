d8e3cd9a1d5d197ac22ebb8f256397c41b76599e
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-06 11:31:05 +0800

    !

--- run.py
+++ run.py
@@ -24,7 +24,6 @@ parser.add_argument('--wandb_name', default='train', type=str, help='|wandb项�
 parser.add_argument('--data_path', default=r'merge.json', type=str, help='|json数据路径|')
 parser.add_argument('--divide', default='9,1', type=str, help='|训练集和验证集划分比例|')
 parser.add_argument('--max_length', default=1024, type=int, help='|模型输入的最大长度|')
-parser.add_argument('--ignore_index', default=-100, type=int, help='|模型输入的标签填充|')
 parser.add_argument('--weight', default='last.pt', type=str, help='|已有模型的位置，没有则新建peft再训练|')
 parser.add_argument('--model', default='llama2', type=str, help='|模型选择|')
 parser.add_argument('--model_path', default='chinese-alpaca-2-1.3b', type=str, help='|原模型位置|')
