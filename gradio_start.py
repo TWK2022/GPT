f7ed65c9914ff2a6d4386a4709e4c0a2166a8c2d
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-07 10:25:35 +0800

    !

--- gradio_start.py
+++ gradio_start.py
@@ -2,25 +2,27 @@
 # 用gradio将程序包装成一个可视化的界面，可以在网页可视化的展示
 import gradio
 import argparse
-from predict import llama_class
+from predict import predict_class
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 设置
 parser = argparse.ArgumentParser('|在服务器上启动gradio服务|')
-parser.add_argument('--model_path', default='chinese-alpaca-2-1.3b', type=str, help='|模型文件夹位置(合并后的模型)|')
-parser.add_argument('--device', default='cpu', type=str, help='|设备|')
+parser.add_argument('--model_path', default='Qwen-1_8B-Chat', type=str, help='|tokenizer和模型文件夹位置|')
+parser.add_argument('--model', default='qwen', type=str, help='|模型类型|')
+parser.add_argument('--temperature', default=0.2, type=float, help='|回答稳定概率，0.2-0.8，越小越稳定|')
+parser.add_argument('--device', default='cuda', type=str, help='|设备|')
 args = parser.parse_args()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 程序
 def function(text):
-    result = model.predict(text)
+    result = model.predict(system='', input_=text)
     return result
 
 
 if __name__ == '__main__':
     print('| 使用gradio启动服务 |')
-    model = llama_class(args)
+    model = predict_class(args)
     gradio_app = gradio.Interface(fn=function, inputs=['text'], outputs=['text'], examples=[['你好呀']])
     gradio_app.launch(share=False)
