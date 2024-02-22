6f2df8657532d7d795547f1e82d02e7ef9dac3b5
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-22 15:49:29 +0800

    !

--- predict.py
+++ predict.py
@@ -12,8 +12,8 @@ class predict_class:
             self.template_add = ' {output_add}</s><s>[INST] {input_add} [/INST]'  # 多轮对话追加的提示模版
             self.split = '[/INST]'
             self.tokenizer = transformers.LlamaTokenizer.from_pretrained(args.model_path)
-            self.eos_token_id = 2
-            self.pad_token_id = 32000
+            self.eos_token_id = 2  # </s>
+            self.pad_token_id = 32000  # <pad>
             self.model = transformers.LlamaForCausalLM.from_pretrained(args.model_path, low_cpu_mem_usage=True,
                                                                        torch_dtype=torch.float16).eval()
         elif args.model == 'baichuan2':
@@ -22,8 +22,8 @@ class predict_class:
             self.template_add = '{output_add}<reserved_106>{input_add}<reserved_107>'  # 多轮对话追加的提示模版
             self.split = '<reserved_107>'
             self.tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-            self.eos_token_id = 2
-            self.pad_token_id = 0
+            self.eos_token_id = 2  # </s>
+            self.pad_token_id = 0  # <unk>
             self.model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True,
                                                                            torch_dtype=torch.float16).eval()
         elif args.model == 'qwen':
@@ -34,8 +34,8 @@ class predict_class:
                                  '<|im_start|>assistant\n')  # 多轮对话追加的提示模版
             self.split = '<|im_start|>assistant\n'
             self.tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-            self.eos_token_id = 151643
-            self.pad_token_id = 151643
+            self.eos_token_id = 151643  # <|endoftext|>
+            self.pad_token_id = 151643  # <|endoftext|>
             self.model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True,
                                                                            torch_dtype=torch.float16).eval()
         if args.peft_model_path:
@@ -47,15 +47,17 @@ class predict_class:
         self.model = self.model.to(args.device)
         self.stream = transformers.TextIteratorStreamer(self.tokenizer)
         self.generation_config = transformers.GenerationConfig(eos_token_id=self.eos_token_id,
-                                                               pad_token_id=self.pad_token_id, max_new_tokens=1024,
-                                                               do_sample=True, temperature=args.temperature,
-                                                               repetition_penalty=args.repetition_penalty)
+                                                               pad_token_id=self.pad_token_id,
+                                                               temperature=args.temperature,
+                                                               max_new_tokens=args.max_new_tokens,
+                                                               repetition_penalty=args.repetition_penalty,
+                                                               do_sample=True)
 
-    def test(self, system, input_, temperature=None, repetition_penalty=None):
-        if temperature:
-            self.generation_config.temperature = temperature
-        if repetition_penalty:
-            self.generation_config.repetition_penalty = repetition_penalty
+    def test(self, system, input_, config_dict=None):
+        if config_dict:
+            self.generation_config.max_new_tokens = config_dict['max_new_tokens']
+            self.generation_config.temperature = config_dict['temperature']
+            self.generation_config.repetition_penalty = config_dict['repetition_penalty']
         with torch.no_grad():
             prompt = self.template.format(system=self.system + system, input=input_)
             input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
@@ -63,11 +65,11 @@ class predict_class:
             result = self.tokenizer.decode(pred[0], skip_special_tokens=True)
         return prompt, result
 
-    def predict(self, system, input_, temperature=None, repetition_penalty=None):
-        if temperature:
-            self.generation_config.temperature = temperature
-        if repetition_penalty:
-            self.generation_config.repetition_penalty = repetition_penalty
+    def predict(self, system, input_, config_dict=None):
+        if config_dict:
+            self.generation_config.max_new_tokens = config_dict['max_new_tokens']
+            self.generation_config.temperature = config_dict['temperature']
+            self.generation_config.repetition_penalty = config_dict['repetition_penalty']
         with torch.no_grad():
             prompt = self.template.format(system=self.system + system, input=input_)
             input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
@@ -76,11 +78,11 @@ class predict_class:
             result = result.split(self.split)[-1]
         return result
 
-    def predict_stream(self, system, input_, temperature=None, repetition_penalty=None):
-        if temperature:
-            self.generation_config.temperature = temperature
-        if repetition_penalty:
-            self.generation_config.repetition_penalty = repetition_penalty
+    def predict_stream(self, system, input_, config_dict=None):
+        if config_dict:
+            self.generation_config.max_new_tokens = config_dict['max_new_tokens']
+            self.generation_config.temperature = config_dict['temperature']
+            self.generation_config.repetition_penalty = config_dict['repetition_penalty']
         with torch.no_grad():
             prompt = self.template.format(system=self.system + system, input=input_)
             input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
@@ -99,10 +101,11 @@ if __name__ == '__main__':
     parser.add_argument('--peft_model_path', default='', type=str, help='|peft模型文件夹位置(空则不使用)|')
     parser.add_argument('--model', default='qwen', type=str, help='|模型类型|')
     parser.add_argument('--system', default='', type=str, help='|追加的系统提示词|')
-    parser.add_argument('--temperature', default=0.2, type=float, help='|回答稳定概率，0.2-0.8，越小越稳定|')
-    parser.add_argument('--repetition_penalty', default=1, type=float, help='|防止模型输出重复的惩罚权重，1为不惩罚|')
+    parser.add_argument('--temperature', default=0.2, type=float, help='|值越小回答稳定概率，0.2-0.8|')
+    parser.add_argument('--max_new_tokens', default=768, type=int, help='|模型最大输出长度限制|')
+    parser.add_argument('--repetition_penalty', default=1.1, type=float, help='|防止模型输出重复的惩罚权重，1为不惩罚|')
     parser.add_argument('--stream', default=False, type=bool, help='|流式输出，需要特殊处理|')
-    parser.add_argument('--device', default='cuda', type=str, help='|设备|')
+    parser.add_argument('--device', default='cpu', type=str, help='|设备|')
     args = parser.parse_args()
     # ---------------------------------------------------------------------------------------------------------------- #
     model = predict_class(args)
