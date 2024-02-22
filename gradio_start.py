7aca8bad5109ffbff4762b186229d7152c58b4f6
TWK <109967047+TWK2022@users.noreply.github.com>
2024-02-22 14:24:49 +0800

    !

--- gradio_start.py
+++ gradio_start.py
@@ -5,22 +5,22 @@ import argparse
 from predict import predict_class
 
 # -------------------------------------------------------------------------------------------------------------------- #
-# 设置
 parser = argparse.ArgumentParser('|在服务器上启动gradio服务|')
 parser.add_argument('--model_path', default='Qwen-1_8B-Chat', type=str, help='|tokenizer和模型文件夹位置|')
 parser.add_argument('--peft_model_path', default='', type=str, help='|peft模型文件夹位置(空则不使用)|')
 parser.add_argument('--model', default='qwen', type=str, help='|模型类型|')
 parser.add_argument('--system', default='', type=str, help='|追加的系统提示词|')
 parser.add_argument('--temperature', default=0.2, type=float, help='|回答稳定概率，0.2-0.8，越小越稳定|')
+parser.add_argument('--repetition_penalty', default=1, type=float, help='|防止模型输出重复的惩罚权重，1为不惩罚|')
 parser.add_argument('--stream', default=True, type=bool, help='|流式输出，需要特殊处理|')
 parser.add_argument('--device', default='cpu', type=str, help='|设备|')
 args = parser.parse_args()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
-# 程序
-def function(input_, temperature, history):
+def function(input_, history, temperature, repetition_penalty):
     temperature = args.temperature if temperature == 'default' else temperature
+    repetition_penalty = args.repetition_penalty if repetition_penalty == 'default' else repetition_penalty
     if args.stream:
         stream = model.predict_stream(system='', input_=input_, temperature=temperature)
         history.append([input_, ''])
@@ -29,18 +29,18 @@ def function(input_, temperature, history):
                 history[-1][-1] += str_
                 yield history
     else:
-        result = model.predict(system='', input_=input_, temperature=temperature)
+        result = model.predict(system='', input_=input_, temperature=temperature, repetition_penalty=repetition_penalty)
         history.append([input_, result])
         yield history
 
 
-if __name__ == '__main__':
-    print('| 使用gradio启动服务 |')
-    model = predict_class(args)
+def gradio_start():
     # 输入
     input_ = gradio.Textbox(placeholder='在此输入内容', label='用户输入', lines=1, scale=9)
-    temperature = gradio.components.Radio(choices=['default', 0.2, 0.4, 0.6, 0.8], value='default',
+    temperature = gradio.components.Radio(choices=['default', 0.2, 0.5, 0.8, 1.0], value='default',
                                           label='temperature')
+    repetition_penalty = gradio.components.Radio(choices=['default', 1.0, 1.1, 1.3, 1.5], value='default',
+                                                 label='repetition_penalty')
     # 输出
     chatbot = gradio.Chatbot(value=[], label=args.model_path, height=450, bubble_full_width=False)
     # 按钮
@@ -59,8 +59,15 @@ if __name__ == '__main__':
             with gradio.Column(scale=1):  # 垂直排列
                 gradio.Markdown('### 设置')
                 temperature.render()
+                repetition_penalty.render()
         # 事件
-        button.click(fn=function, inputs=[input_, temperature, chatbot], outputs=[chatbot])
+        button.click(fn=function, inputs=[input_, chatbot, temperature, repetition_penalty], outputs=[chatbot])
     # 启动
     gradio_app.queue()
     gradio_app.launch(share=False)
+
+
+if __name__ == '__main__':
+    print('| 使用gradio启动服务 |')
+    model = predict_class(args)
+    gradio_start()
