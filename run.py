90e5a6c6ca7b333a1a8951fc9747e600e93044c1
TWK <109967047+TWK2022@users.noreply.github.com>
2024-03-08 10:40:49 +0800

    !

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ parser = argparse.ArgumentParser(description='|大模型微调:peft模型训练|
 parser.add_argument('--wandb', default=False, type=bool, help='|是否使用wandb可视化|')
 parser.add_argument('--wandb_project', default='GPT', type=str, help='|wandb项目名称|')
 parser.add_argument('--wandb_name', default='train', type=str, help='|wandb项目中的训练名称|')
-parser.add_argument('--data_path', default=r'merge.json', type=str, help='|json数据路径|')
+parser.add_argument('--data_path', default=r'data_demo.json', type=str, help='|json数据路径|')
 parser.add_argument('--divide', default='99,1', type=str, help='|训练集和验证集划分比例|')
 parser.add_argument('--max_length', default=768, type=int, help='|模型输入的最大长度|')
 parser.add_argument('--weight', default='last.pt', type=str, help='|已有模型的位置，没有则新建peft再训练|')
