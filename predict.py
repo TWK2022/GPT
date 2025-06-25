62e84a1ea14fe3ba5ed1248c683ea5e195527e06
twk <1024565378@qq.com>
2025-06-25 17:45:09 +0800

    !

--- predict.py
+++ predict.py
@@ -1,45 +1,45 @@
+import os
 import peft
 import torch
+import argparse
 import threading
 import transformers
 
+# -------------------------------------------------------------------------------------------------------------------- #
+parser = argparse.ArgumentParser('|模型预测|')
+parser.add_argument('--model_path', default='qwen3_0.6b', type=str, help='|tokenizer和模型文件夹位置|')
+parser.add_argument('--peft_model_path', default='', type=str, help='|peft模型文件夹位置(空则不使用)|')
+parser.add_argument('--model', default='qwen3', type=str, help='|模型类型|')
+parser.add_argument('--system', default='', type=str, help='|追加的系统提示词|')
+parser.add_argument('--think', default=False, type=bool, help='|思维链|')
+parser.add_argument('--temperature', default=0.2, type=float, help='|值越小回答越稳定，0.2-0.8|')
+parser.add_argument('--max_new_tokens', default=768, type=int, help='|模型最大输出长度限制|')
+parser.add_argument('--repetition_penalty', default=1.1, type=float, help='|防止模型输出重复的惩罚权重，1为不惩罚|')
+parser.add_argument('--stream', default=False, type=bool, help='|流式输出|')
+parser.add_argument('--device', default='cuda', type=str, help='|设备|')
+args, _ = parser.parse_known_args()  # 防止传入参数冲突，替代args = parser.parse_args()
+args.device = 'cpu' if not torch.cuda.is_available() else args.device
 
+
+# -------------------------------------------------------------------------------------------------------------------- #
 class predict_class:
-    def __init__(self, args):
+    def __init__(self, args=args):
+        self.think = args.think
         self.model_type = args.model
-        if args.model == 'llama2':
-            self.system = 'You are a helpful assistant. 你是一个乐于助人的助手。'  # 默认系统提示
-            self.template = ('<s>[INST] <<SYS>>\n{system}\n<</SYS>>\n\n{input} [/INST]')  # 单轮对话提示模版
-            self.template_add = ' {output_add}</s><s>[INST] {input_add} [/INST]'  # 多轮对话追加的提示模版
-            self.strip_end = 4  # </s>
-            self.tokenizer = transformers.LlamaTokenizer.from_pretrained(args.model_path)
-            self.eos_token_id = 2  # </s>
-            self.pad_token_id = 32000  # <pad>
-            self.model = transformers.LlamaForCausalLM.from_pretrained(args.model_path, low_cpu_mem_usage=True,
-                                                                       torch_dtype=torch.float16).eval()
-        elif args.model == 'baichuan2':
+        if args.model == 'qwen3':
             self.system = ''  # 默认系统提示
-            self.template = '{system}<reserved_106>{input}<reserved_107>'  # 单轮对话提示模版
-            self.template_add = '{output_add}<reserved_106>{input_add}<reserved_107>'  # 多轮对话追加的提示模版
-            self.strip_end = 4  # </s>
-            self.tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
-            self.eos_token_id = 2  # </s>
-            self.pad_token_id = 0  # <unk>
-            self.model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True,
-                                                                           torch_dtype=torch.float16).eval()
-        elif args.model == 'qwen':
-            self.system = 'You are a helpful assistant.\n'  # 默认系统提示
             self.template = ('<|im_start|>system\n{system}<|im_end|>\n<|im_start|>user\n{input}<|im_end|>\n'
                              '<|im_start|>assistant\n')  # 单轮对话提示模版
-            self.template_add = ('{output_add}<|im_end|>\n<|im_start|>user\n{input}<|im_end|>\n'
-                                 '<|im_start|>assistant\n')  # 多轮对话追加的提示模版
-            self.strip_end = 10  # <|im_end|>
-            self.tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+            self.template_think = '<think>\n\n<think>\n\n'  # 思维链
+            self.template_history = ('{output_add}<|im_end|>\n<|im_start|>user\n{input}<|im_end|>\n'
+                                     '<|im_start|>assistant\n')  # 多轮对话追加的提示模版
+            self.bos_token_id = 151644  # <|im_start|>
             self.eos_token_id = 151645  # <|im_end|>
             self.pad_token_id = 151643  # <|endoftext|>
-            self.model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True,
-                                                                           torch_dtype=torch.float16).eval()
-        if args.peft_model_path:
+        self.tokenizer = transformers.AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+        self.model = transformers.AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True,
+                                                                       torch_dtype=torch.float16).eval()
+        if os.path.exists(args.peft_model_path):
             self.model = peft.PeftModel.from_pretrained(self.model, args.peft_model_path)
         self.device = args.device
         self.model = self.model.float() if args.device.lower() == 'cpu' else self.model.half()
@@ -52,104 +52,36 @@ class predict_class:
                                                                repetition_penalty=args.repetition_penalty,
                                                                do_sample=True)
 
-    def test(self, system, input_, config_dict=None):
+    def predict(self, input_, system='', config_dict=None):
         if config_dict:
             self.generation_config.max_new_tokens = config_dict['max_new_tokens']
             self.generation_config.temperature = config_dict['temperature']
             self.generation_config.repetition_penalty = config_dict['repetition_penalty']
         with torch.no_grad():
             prompt = self.template.format(system=self.system + system, input=input_)
+            prompt += self.template_think if not self.think else ''
             input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
             pred = self.model.generate(input_ids=input_ids, generation_config=self.generation_config)
-            result = self.tokenizer.decode(pred[0])
-            result = result[len(prompt):-self.strip_end].strip()
-        return prompt, result
-
-    def predict(self, system, input_, config_dict=None):
-        if config_dict:
-            self.generation_config.max_new_tokens = config_dict['max_new_tokens']
-            self.generation_config.temperature = config_dict['temperature']
-            self.generation_config.repetition_penalty = config_dict['repetition_penalty']
-        with torch.no_grad():
-            prompt = self.template.format(system=self.system + system, input=input_)
-            input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
-            pred = self.model.generate(input_ids=input_ids, generation_config=self.generation_config)
-            result = self.tokenizer.decode(pred[0])
-            result = result[len(prompt):-self.strip_end].strip()
+            result = self.tokenizer.decode(pred[0][len(input_ids[0]):], skip_special_tokens=True)
         return result
 
-    def predict_stream(self, system, input_, config_dict=None):
+    def predict_stream(self, input_, system='', config_dict=None):
         if config_dict:
             self.generation_config.max_new_tokens = config_dict['max_new_tokens']
             self.generation_config.temperature = config_dict['temperature']
             self.generation_config.repetition_penalty = config_dict['repetition_penalty']
         with torch.no_grad():
             prompt = self.template.format(system=self.system + system, input=input_)
+            prompt += self.template_think if not self.think else ''
             input_ids = self.tokenizer.encode(prompt, add_special_tokens=False, return_tensors='pt').to(self.device)
             kwargs = {'input_ids': input_ids, 'generation_config': self.generation_config, 'streamer': self.stream}
             thread = threading.Thread(target=self.model.generate, kwargs=kwargs)
             thread.start()
-            return eval(f'self._stream_{self.model_type}()')
-
-    def _stream_llama2(self):
-        for index, str_ in enumerate(self.stream):
-            if index == 0:
-                continue
-            elif index == 1:
-                str_ = str_.replace('[/INST]', '').strip()
-            elif str_[-4:] == '</s>':
-                str_ = str_[1:-4]
-            yield str_
-
-    def _stream_baichuan2(self):
-        for index, str_ in enumerate(self.stream):
-            if index == 0:
-                continue
-            elif index == 1:
-                str_ = str_.split('<reserved_107>')[-1]
-            elif str_[-4:] == '</s>':
-                str_ = str_[1:-4]
-            yield str_
-
-    def _stream_qwen(self):
-        for index, str_ in enumerate(self.stream):
-            if index == 0:
-                continue
-            elif str_[-10:] == '<|im_end|>':
-                str_ = str_.replace('<|im_end|>', '')
-            yield str_
+            for str_ in self.stream:
+                yield str_
 
 
 if __name__ == '__main__':
-    import argparse
-
-    # ---------------------------------------------------------------------------------------------------------------- #
-    parser = argparse.ArgumentParser('|模型预测|')
-    parser.add_argument('--model_path', default='Qwen-1_8B-Chat', type=str, help='|tokenizer和模型文件夹位置|')
-    parser.add_argument('--peft_model_path', default='', type=str, help='|peft模型文件夹位置(空则不使用)|')
-    parser.add_argument('--model', default='qwen', type=str, help='|模型类型|')
-    parser.add_argument('--system', default='', type=str, help='|追加的系统提示词|')
-    parser.add_argument('--temperature', default=0.2, type=float, help='|值越小回答越稳定，0.2-0.8|')
-    parser.add_argument('--max_new_tokens', default=768, type=int, help='|模型最大输出长度限制|')
-    parser.add_argument('--repetition_penalty', default=1.1, type=float, help='|防止模型输出重复的惩罚权重，1为不惩罚|')
-    parser.add_argument('--stream', default=False, type=bool, help='|流式输出|')
-    parser.add_argument('--device', default='cuda', type=str, help='|设备|')
-    args = parser.parse_args()
-    # ---------------------------------------------------------------------------------------------------------------- #
-    model = predict_class(args)
-    if args.stream:
-        while True:
-            system = args.system
-            input_ = input('用户输入：').strip()
-            stream = model.predict_stream(system, input_)
-            print(f'----------stream----------')
-            for str_ in stream:
-                print(str_, end='')
-            print('\n')
-    else:
-        while True:
-            system = args.system
-            input_ = input('用户输入：').strip()
-            prompt, result = model.test(system, input_)
-            print(f'----------prompt----------\n{prompt}')
-            print(f'----------result----------\n{result}')
+    model = predict_class()
+    for str_ in model.predict_stream('你是谁，精简回答'):
+        print(str_, end='')
--- run.py
+++ run.py
@@ -1,83 +1,67 @@
-import os
 import wandb
 import torch
 import argparse
-from block.data_get import data_get
-from block.model_get import model_get
-from block.train_get import train_get
+from train_class import train_class
 
 # -------------------------------------------------------------------------------------------------------------------- #
 # 单轮对话数据格式(json)：没有system时用默认的
 # [{'input':'B','output':'C'},...]
 # [{'system':'A','input':'B','output':'C'},...]
 # -------------------------------------------------------------------------------------------------------------------- #
-# 分布式数据并行训练：
+# 分布式数据并行训练:
 # python -m torch.distributed.launch --master_port 9999 --nproc_per_node n run.py --distributed True
-# master_port为GPU之间的通讯端口，空闲的即可
-# n为GPU数量
+# master_port为gpu之间的通讯端口，空闲的即可。n为gpu数量
 # -------------------------------------------------------------------------------------------------------------------- #
 parser = argparse.ArgumentParser(description='|大模型微调:peft模型训练|')
-parser.add_argument('--wandb', default=False, type=bool, help='|是否使用wandb可视化|')
-parser.add_argument('--wandb_project', default='GPT', type=str, help='|wandb项目名称|')
-parser.add_argument('--wandb_name', default='train', type=str, help='|wandb项目中的训练名称|')
-parser.add_argument('--data_path', default=r'data_demo.json', type=str, help='|json数据路径|')
-parser.add_argument('--divide', default='99,1', type=str, help='|训练集和验证集划分比例|')
-parser.add_argument('--max_length', default=768, type=int, help='|模型输入的最大长度|')
-parser.add_argument('--weight', default='last.pt', type=str, help='|已有模型的位置，没有则新建peft再训练|')
-parser.add_argument('--model', default='qwen', type=str, help='|模型选择|')
-parser.add_argument('--model_path', default='Qwen-1_8B-Chat', type=str, help='|原模型位置|')
-parser.add_argument('--save_pt', default=0, type=int, help='|每几轮保存一次last.pt模型以便中断后继续训练，0为不保存|')
-parser.add_argument('--epoch', default=30, type=int, help='|训练总轮数(包含之前已训练轮数)|')
-parser.add_argument('--batch', default=2, type=int, help='|训练批量大小，分布式时为总批量|')
+parser.add_argument('--log', default=True, type=bool, help='|日志|')
+parser.add_argument('--print_info', default=True, type=bool, help='|打印信息|')
+parser.add_argument('--wandb', default=False, type=bool, help='|wandb可视化|')
+parser.add_argument('--data_path', default='dataset/data_demo.json', type=str, help='|数据位置|')
+parser.add_argument('--divide', default=(19, 1), type=tuple, help='|训练集和验证集划分比例|')
+parser.add_argument('--weight_path', default='last.pt', type=str, help='|加载模型，优先级:加载模型>剪枝训练>创建新模型|')
+parser.add_argument('--weight_again', default=True, type=bool, help='|重置学习率等状态，在weight_path上重新训练|')
+parser.add_argument('--model', default='qwen3', type=str, help='|模型选择|')
+parser.add_argument('--model_path', default='qwen3_0.6b', type=str, help='|原模型位置|')
+parser.add_argument('--save_epoch', default=5, type=int, help='|每x轮和最后一轮保存完整模型|')
+parser.add_argument('--save_path', default='last.pt', type=str, help='|保存模型|')
+parser.add_argument('--max_length', default=768, type=int, help='|模型输入+输出最大长度|')
+parser.add_argument('--epoch', default=10, type=int, help='|训练总轮数(包含之前已训练轮数)|')
+parser.add_argument('--batch', default=1, type=int, help='|训练批量大小，分布式时为总批量|')
 parser.add_argument('--warmup_ratio', default=0.01, type=float, help='|预热训练步数占总步数比例，最少5步，基准为0.01|')
-parser.add_argument('--lr_start', default=0.0001, type=float, help='|初始学习率，adam算法，批量大时要增大，基准为0.0001|')
-parser.add_argument('--lr_end_ratio', default=0.01, type=float, help='|最终学习率=lr_end_ratio*lr_start，基准为0.01|')
-parser.add_argument('--lr_end_epoch', default=30, type=int, help='|最终学习率达到的轮数，每一步都调整，余玄下降法|')
+parser.add_argument('--lr_start', default=0.0001, type=float, help='|初始学习率，adam算法，批量小时要减小，基准为0.001|')
+parser.add_argument('--lr_end_ratio', default=0.1, type=float, help='|最终学习率=lr_end_ratio*lr_start，基准为0.01|')
+parser.add_argument('--lr_end_epoch', default=10, type=int, help='|最终学习率达到的轮数，每一步都调整，余玄下降法|')
 parser.add_argument('--regularization', default='L2', type=str, help='|正则化，有L2、None|')
-parser.add_argument('--r_value', default=0.0002, type=float, help='|正则化权重系数，可从0.0001开始逐渐增加，直到最佳值|')
-parser.add_argument('--device', default='cuda', type=str, help='|训练设备|')
-parser.add_argument('--latch', default=False, type=bool, help='|模型和数据是否为锁存，True为锁存|')
-parser.add_argument('--num_worker', default=0, type=int, help='|CPU处理数据的进程数，0只有一个主进程，一般为0、2、4、8|')
-parser.add_argument('--ema', default=False, type=bool, help='|使用平均指数移动(EMA)调整参数|')
-parser.add_argument('--amp', default=True, type=bool, help='|混合float16精度训练，CPU时不可用|')
+parser.add_argument('--r_value', default=0.0005, type=float, help='|正则化权重系数，基准为0.0005|')
+parser.add_argument('--device', default='cuda', type=str, help='|设备|')
+parser.add_argument('--latch', default=True, type=bool, help='|模型和数据是否为锁存|')
+parser.add_argument('--num_worker', default=0, type=int, help='|cpu处理数据进程数，0为一个主进程，一般为0、2、4、8|')
+parser.add_argument('--ema', default=False, type=bool, help='|平均指数移动(EMA)调整参数|')
+parser.add_argument('--amp', default=True, type=bool, help='|混合float16精度训练，cpu时不可用，出现nan可能与gpu有关|')
 parser.add_argument('--distributed', default=False, type=bool, help='|单机多卡分布式训练，分布式训练时batch为总batch|')
 parser.add_argument('--local_rank', default=0, type=int, help='|分布式训练使用命令后会自动传入的参数|')
 args = parser.parse_args()
-args.divide = list(map(int, args.divide.split(',')))
-args.device_number = max(torch.cuda.device_count(), 1)  # 使用的GPU数，可能为CPU
-# 为CPU设置随机种子
-torch.manual_seed(999)
-# 为所有GPU设置随机种子
-torch.cuda.manual_seed_all(999)
-# 固定每次返回的卷积算法
-torch.backends.cudnn.deterministic = True
-# cuDNN使用非确定性算法
-torch.backends.cudnn.enabled = True
-# 训练前cuDNN会先搜寻每个卷积层最适合实现它的卷积算法，加速运行；但对于复杂变化的输入数据，可能会有过长的搜寻时间，对于训练比较快的网络建议设为False
-torch.backends.cudnn.benchmark = True
+if not torch.cuda.is_available():  # 没有gpu
+    args.device = 'cpu'
+    args.amp = False
+args.device_number = max(torch.cuda.device_count(), 1)  # 使用的gpu数，可能为cpu
 # wandb可视化:https://wandb.ai
 if args.wandb and args.local_rank == 0:  # 分布式时只记录一次wandb
-    args.wandb_run = wandb.init(project=args.wandb_project, name=args.wandb_name, config=args)
+    args.wandb_run = wandb.init(project='GPT', name='train', config=args)
 # 混合float16精度训练
 if args.amp:
     args.amp = torch.cuda.amp.GradScaler()
 # 分布式训练
 if args.distributed:
-    torch.distributed.init_process_group(backend='nccl')
-    args.device = torch.device("cuda", args.local_rank)
-# -------------------------------------------------------------------------------------------------------------------- #
-assert os.path.exists(args.data_path), f'! data_path不存在:{args.data_path} !'
-if os.path.exists(args.weight):
-    print(f'| 加载模型继续训练:{args.weight} |')
-else:
-    print(f'| 创建peft模型以微调模型:{args.model_path} |')
+    torch.distributed.init_process_group(backend='nccl')  # 分布式训练初始化
+    args.device = torch.device('cuda', args.local_rank)
+# 设置
+torch.manual_seed(999)  # 为cpu设置随机种子
+torch.cuda.manual_seed_all(999)  # 为所有gpu设置随机种子
+torch.backends.cudnn.deterministic = True  # 固定每次返回的卷积算法
+torch.backends.cudnn.enabled = True  # cuDNN使用非确定性算法
+torch.backends.cudnn.benchmark = False  # 训练前cuDNN会先搜寻每个卷积层最适合实现它的卷积算法，加速运行；但对于复杂变化的输入数据，可能会有过长的搜寻时间，对于训练比较快的网络建议设为False
 # -------------------------------------------------------------------------------------------------------------------- #
 if __name__ == '__main__':
-    # 摘要
-    print(f'| args:{args} |') if args.local_rank == 0 else None
-    # 数据
-    data_dict = data_get(args)
-    # 模型
-    model_dict = model_get(args)
-    # 训练
-    train_get(args, data_dict, model_dict)
+    train = train_class(args)
+    train.train()
